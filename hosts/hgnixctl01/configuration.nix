53264533e7ca0299f17a05548051432fcef3b016
ChristopherBStocks <christopher.b.stocks@gmail.com>
2025-06-17 03:41:56 +0100

    feat: add static ip to hgnixctl01 and hgnixctl02

--- hosts/hgnixctl01/configuration.nix
+++ hosts/hgnixctl01/configuration.nix
@@ -4,6 +4,7 @@
   imports = [
     ./hardware-configuration.nix
     ./disko.nix
+    ./static-ip.nix
     ../../configs/hydrogroup-server-colmena.nix
   ];
   system.stateVersion                   = "25.05";
