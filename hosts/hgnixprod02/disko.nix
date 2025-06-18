835189e2f12a8bcc8fcb8d772e83e5b71ce17931
ChristopherBStocks <christopher.b.stocks@gmail.com>
2025-06-18 12:51:42 +0100

    feat: migrate hyper-v disko to use disk path

--- hosts/hgnixprod02/disko.nix
+++ hosts/hgnixprod02/disko.nix
@@ -1,90 +1,80 @@
 { lib, ... }:
 {
-  disko.devices = {
-    disk.sda = {
-      device = "/dev/sda";
-      type = "disk";
+    disko.devices = {
 
-      content = {
-        type = "gpt";
-        partitions = {
-
-          root = {
-            size    = "100%";
-            type    = "8300";
-            content = {
-              type        = "filesystem";
-              format      = "ext4";
-              mountpoint  = "/srv/data/ssd";
-              mountOptions = [ "noatime" "discard" ];
+      os = {
+        type   = "disk";
+        device = "/dev/disk/by-path/acpi-VMBUS:00-vmbus-acc06c62e8ca4e5ca460cf6589ccb067-lun-0";
+        content = {
+          type   = "gpt";
+          partitions = {
+            esp = {
+              type  = "EF00";
+              size  = "512M";
+              content = {
+                type       = "filesystem";
+                format     = "vfat";
+                mountpoint = "/boot/efi";
+                mountOptions = [ "umask=0077" "noatime" ];
+              };
+            };
+            root = {
+              type  = "8300";
+              size  = "remaining";
+              content = {
+                type       = "filesystem";
+                format     = "ext4";
+                mountpoint = "/";
+                mountOptions = [ "noatime" "discard" ];
+              };
             };
           };
-
         };
       };
-    };
 
-    disk.sdb = {
-      device = "/dev/sdb";
-      type = "disk";
-
-      content = {
-        type = "gpt";
-        partitions = {
-
-          root = {
-            size    = "100%";
-            type    = "8300";
-            content = {
-              type        = "filesystem";
-              format      = "ext4";
-              mountpoint  = "/srv/data/hdd";
-              mountOptions = [ "noatime" "discard" ];
+      ssd-data = {
+        type   = "disk";
+        device = "/dev/disk/by-path/acpi-VMBUS:00-vmbus-acc06c62e8ca4e5ca460cf6589ccb067-lun-1";
+        content = {
+          type = "gpt";
+          partitions = {
+            data = {
+              size    = "100%";
+              type    = "8300";
+              content = {
+                type        = "filesystem";
+                format      = "ext4";
+                mountpoint  = "/srv/data/sdd";
+                mountOptions = [ "noatime" "discard" ];
+              };
             };
           };
-
         };
-      };
-    };
+      }
 
-    disk.sdc = {
-      device = "/dev/sdc";
-      type   = "disk";
-
-      content = {
-        type = "gpt";
-        partitions = {
-
-          esp = {
-            size       = "1G";
-            type       = "EF00";
-            content = {
-              type      = "filesystem";
-              format    = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "umask=0077" "noatime" ];
+      hdd-data = {
+        type   = "disk";
+        device = "/dev/disk/by-path/acpi-VMBUS:00-vmbus-acc06c62e8ca4e5ca460cf6589ccb067-lun-2";
+        content = {
+          type = "gpt";
+          partitions = {
+            data = {
+              size    = "100%";
+              type    = "8300";
+              content = {
+                type        = "filesystem";
+                format      = "ext4";
+                mountpoint  = "/srv/data/hdd";
+                mountOptions = [ "noatime" "discard" ];
+              };
             };
           };
-
-          root = {
-            size    = "100%";
-            type    = "8300";
-            content = {
-              type       = "filesystem";
-              format     = "ext4";
-              mountpoint = "/";
-              mountOptions = [ "noatime" "discard" ];
-            };
-          };
-
         };
-      };
+      }
     };
 
-  };
-
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-  };
+    boot.loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
 }
